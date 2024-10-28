b074c596ef6fbf08f21064527e32a6af1c1212a2
madhavpai09 <madhav09pai@gmail.com>
2024-10-28 15:55:40 +0530

    added new stack w array program

---
+++ stack_aray.c
@@ -0,0 +1,76 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+
+struct Stack { 
+    int top; 
+    unsigned capacity; 
+    int* array; 
+}; 
+
+struct Stack* createStack(unsigned capacity) 
+{ 
+    struct Stack* stack = (struct Stack*)malloc(sizeof(struct Stack)); 
+    stack->capacity = capacity; 
+    stack->top = -1; 
+    stack->array = (int*)malloc(stack->capacity * sizeof(int)); 
+    return stack; 
+} 
+
+
+int isFull(struct Stack* stack) 
+{ 
+    return stack->top == stack->capacity - 1; 
+} 
+
+
+int isEmpty(struct Stack* stack) 
+{ 
+    return stack->top == -1; 
+} 
+
+void push(struct Stack* stack, int item) 
+{ 
+    if (isFull(stack)) 
+        return; 
+    stack->array[++stack->top] = item; 
+    printf("%d pushed to stack\n", item); 
+} 
+
+int pop(struct Stack* stack) 
+{ 
+    if (isEmpty(stack)) 
+        return 0; 
+    return stack->array[stack->top--]; 
+} 
+
+
+int peek(struct Stack* stack) 
+{ 
+    if (isEmpty(stack)) 
+        return 0; 
+    return stack->array[stack->top]; 
+} 
+
+int main() 
+{ 
+    struct Stack* stack = createStack(100); 
+    int choice=0,n;
+    while(choice<=3)
+    {
+    printf("\nEnter: \n1.Push \n2.Pop \n3.Display\n\nEnter any negative number to exit.");
+    scanf("%d",&choice);
+    switch (choice)
+    {
+        case 1: printf("Enter your value");
+                scanf("top element: %d",&n);
+                push(stack,n);
+                break;
+        case 2: pop(stack);
+                break;
+        case 3: printf("%d",peek(stack));
+                break;
+        default:exit(0);
+    }
+    }
+    return 0;
+} 
